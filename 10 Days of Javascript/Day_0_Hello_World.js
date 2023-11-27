016b82bf9f4c42f2c8d5d31b821a5d768fb31af8
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2023-11-27 14:06:47 -0300

    `Day 0`: first challenge completed!

---
+++ 10 Days of Javascript/Day_0_Hello_World.js
@@ -0,0 +1,9 @@
+// Desafio disponível em https://www.hackerrank.com/challenges/js10-hello-world/
+
+function greeting(parameterVariable) {
+  // This line prints 'Hello, World!' to the console:
+  console.log("Hello, World!");
+
+  // Write a line of code that prints parameterVariable to stdout using console.log:
+  console.log(parameterVariable);
+}
