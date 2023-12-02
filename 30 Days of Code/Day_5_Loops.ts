4bc8c616faff78151ffe6abc065aefc29b28d3aa
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2023-12-02 20:21:02 -0300

    `Day 5 of 30`: completed!

---
+++ 30 Days of Code/Day_5_Loops.ts
@@ -0,0 +1,33 @@
+// Desafio disponível em https://www.hackerrank.com/challenges/30-loops
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString: string = "";
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+
+  main();
+});
+
+function readLine(): string {
+  return inputLines[currentLine++];
+}
+
+function main() {
+  const n: number = parseInt(readLine().trim(), 10);
+
+  for (let i: number = 1; i < 11; i++) {
+    console.log(`${n} x ${i} = ${n * i}`);
+  }
+}
